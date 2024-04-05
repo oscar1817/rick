4c7eda13c887571cfe81c295e6d2d5b0985968c9
Misyblus23 <mdcujia@misena.edu.co>
2024-04-05 10:15:05 -0500

    Rick y morty

--- js/app.js
+++ js/app.js
@@ -3,17 +3,23 @@ function buscar() {
   valor = document.getElementById("pokemon").value
   cadena = ""
   //conexion a la api o ruta
-  url = "https://rickandmortyapi.com/api/character/" + valor
+  url = "https://rickandmortyapi.com/api/character/?name=" + valor
   fetch(url)
     .then(response => response.json())
     .then(data => visualizar(data))
     .catch(error => console.log(error))
   const visualizar = (data => {
     console.log(data)
-    cadena = `<strong>id:</strong> ${data.id} `
-    cadena += `<strong>Nombre:</strong> ${data.name} `
-    cadena += `<img src=${data.image} width="300" height="300"></img>`
+    for (let i = 0; i < data.results.length; i++) {
+      //cadena = `<strong>id:</strong> ${data.id} `
+      cadena += `<div>`
+      cadena += `<strong>Nombre:</strong> ${data.results[i].name} <br>`
+      cadena += `<strong>Estado:</strong> ${data.results[i].status} <br>`
+      cadena += `<strong>Especies:</strong> ${data.results[i].species} <br>`
+      cadena += `<strong>Genero:</strong> ${data.results[i].gender} <br>`
+      cadena += `<img src=${data.results[i].image} width="300" height="300"></img> <br>`
+      cadena += `</div>`
+    }
     document.getElementById("contenido").innerHTML = cadena
-
   })
 }
